9ae8fa0c2bebadd572d75ffcb0bd293e3e5c33f6
Sergio <ss.gutierrez10@uniandes.edu.co>
2017-02-12 16:56:45 -0500

    cambio

--- t1/Entrega.c
+++ t1/Entrega.c
@@ -36,7 +36,7 @@ MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 int up, down,a,left, right, x0, x1, y0, y1, i=1, j=1, n=0;
 double average;
 
-a= m*m/world_size;
+a= round(m*m/world_size);
 m = L/h;
 N = 2*m*m;
 
@@ -51,29 +51,34 @@ double *V_sub = malloc(a*sizeof(double));
 double *V = malloc(m*m*sizeof(double));
 double *V1 = malloc(m*m*sizeof(double));
 
-while(n,N)
+while(n<N)
 {
 	V= init(x0,x1,y0,y1,V);
 	
 	if (world_rank == 0)
 	{
-		int p=0;
+		
 		for(i=0 ; i <m+a+1 ; i++)
 		{
 			V_sub[p]=V[i];
-			p+=1;
+			
 		}
-	
-		int q=0;
-		for(i=m+1 ; i<a+1 ; i++)
-		{
-			up=i-m;
-			down=i+m;
-			left=i-1;
-			right=i+1;
-			average = (V_sub[up]+ V_sub[down] + V_sub[left] + V_sub[right])/4;
-			V_new[q]=average;
-			q+=1;
+
+		for(i=0 ; i<a+1 ; i++)
+		{	
+			if(!(i>m+1))
+				{
+				up=i-m;
+				down=i+m;
+				left=i-1;
+				right=i+1;
+				average = (V_sub[up]+ V_sub[down] + V_sub[left] + V_sub[right])/4;
+				V_new[q]=average;
+				}
+			else
+				{	
+				V_new[i]= V_sub[i];	
+				}					
 		} 
 	}
 
@@ -104,14 +109,14 @@ while(n,N)
 	if(world_rank==world_size)
 	{
 		int h=0;
-		for(i=a*world_rank-m ; i <m*m-m ; i++)
+		for(i=a*world_rank-m ; i <m*m ; i++)
 		{
 			V_sub[h]=V[i];
 			h+=1;
 		}
 	
 		int k=0;
-		for(i=m+1 ; i<a+1 ; i++)
+		for(i=m ; i<a+1 ; i++)
 		{
 			up=i-m;
 			down=i+m;
@@ -124,12 +129,18 @@ while(n,N)
 	}
 	
 	MPI_Allgather(V_new,a,MPI_DOUBLE,V1,a,MPI_DOUBLE,MPI_COMM_WORLD);
+	for(i=0;i<m*m;i++)
+	{
+	V[i]=V1[i]
+	}
 	n=+1;	
 }	
+
 for(i=0 ; i <m*m ;i++)
 {
 	printf("%f\n", V[i]);
 }
+MPI_Barrier(MPI_COMM_WORLD);
 MPI_Finalize();
 return 0;
 }
